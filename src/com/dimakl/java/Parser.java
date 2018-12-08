26e98f9beff27bf0a20a32bb9f3de0d1ff4dab85
Dmitrii Kocheshkov <dimakid123@gmail.com>
2018-12-08 23:44:11 +0300

    add part of antlr code

--- src/com/dimakl/java/Parser.java
+++ src/com/dimakl/java/Parser.java
@@ -1,14 +1,23 @@
 package com.dimakl.java;
 
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.Lexer;
-import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.*;
+import com.dimakl.antlr.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 
 public class Parser {
 
     public static void main(String[] args) {
-        // write your code here
+        String input = "A and B = 1";
+        CharStream stream = CharStreams.fromString(input);
+        // To re-use the parser+lexer instances, call their 'reset()' methods after setting their input streams.
+        LogExpLexer lexer = new LogExpLexer(stream);
+        TokenStream tokenStream =  new CommonTokenStream(lexer) ;
+        LogExpParser logExpParser = new LogExpParser(tokenStream);
+        LogExpParser.ParseContext parseContext =  logExpParser.parse();
+        System.out.println(parseContext);
     }
 }
