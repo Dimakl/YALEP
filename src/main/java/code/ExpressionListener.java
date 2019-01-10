d1ff5aeaae6d24aeab559bd284a7e35635f2df59
Dimakl <dimakid123@gmail.com>
2019-01-11 01:23:52 +0300

    add print method and add logic in exitBinExp

--- src/main/java/code/ExpressionListener.java
+++ src/main/java/code/ExpressionListener.java
@@ -20,6 +20,8 @@ public class ExpressionListener extends LogExpBaseListener{
 
     // such a redundancy is made for future development
     public final String PASS_NODE = "pass";
+    public final String CREATE_NODE = "create";
+    public final String DEAD_NODE = "dead";
 
     public ExpressionListener(LogExpLexer lexer){
         this.lexer = lexer;
@@ -30,33 +32,55 @@ public class ExpressionListener extends LogExpBaseListener{
         return new ArrayList<String>(vars);
     }
 
+    // method for testing TODO: delete this method
+    private void printNodeMap(){
+        for (String item : nodeMap.keySet()){
+            System.out.print(item+": ");
+            for (String nodePos : nodeMap.get(item))
+                System.out.print(nodePos+" ");
+            System.out.println();
+        }
+    }
+
 
     @Override
     public void exitBinaryExpression(LogExpParser.BinaryExpressionContext ctx) {
         //TODO: understand what must be happening here
         Vocabulary vocabulary =  lexer.getVocabulary();
         String leftOp = ctx.left.getText(),
-            rightOp = ctx.right.getText(),
-            op = ctx.op.getText(),
-            fullOpName = "("+leftOp+")" + op + "("+rightOp+")";
+               rightOp = ctx.right.getText(),
+               op = ctx.op.getText(),
+               fullOpName = "("+leftOp+")" + op + "("+rightOp+")";
+
         if (!nodeMap.containsKey(fullOpName))
             nodeMap.put(fullOpName, new ArrayList<String>());
         else {
             int i;
             for (i = 2; i < 20; i++)
                 if (!nodeMap.containsKey(fullOpName+"["+i+"]")) {
-                    fullOpName = fullOpName+"["+i+"]";
+                    fullOpName = fullOpName + "[" + i + "]";
                     nodeMap.put(fullOpName, new ArrayList<>());
                     break;
                 }
         }
+        int leftLen = nodeMap.get(leftOp).size(),
+            rightLen = nodeMap.get(rightOp).size();
+        int maxLen = ( leftLen > rightLen) ? leftLen : rightLen;
+        nodeMap.get(leftOp).addAll(Collections.nCopies(maxLen - leftLen, PASS_NODE));
+        nodeMap.get(rightOp).addAll(Collections.nCopies(maxLen - rightLen, PASS_NODE));
+        nodeMap.get(fullOpName).addAll(Collections.nCopies(maxLen, PASS_NODE));
+        printNodeMap();
     }
 
 
     //TODO: make realization of these 2 methods:
     @Override
     public void exitIdentifierExpression(LogExpParser.IdentifierExpressionContext ctx) {
-        nodeMap.putIfAbsent(ctx.id.getText(), new ArrayList<String>());
+        nodeMap.putIfAbsent(ctx.id.getText(),
+                new ArrayList<String>() {{
+                    add(CREATE_NODE);
+                }}
+        );
     }
 
     @Override
