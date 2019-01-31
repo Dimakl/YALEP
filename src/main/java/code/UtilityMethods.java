93dd0c065b3cccac4121e4e23abe2c9e42695bad
Dimakl <dimakid123@gmail.com>
2019-01-31 23:37:36 +0300

    add search methods

--- src/main/java/code/UtilityMethods.java
+++ src/main/java/code/UtilityMethods.java
@@ -1,5 +1,8 @@
 package code;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class UtilityMethods {
 
     public static String deleteBrackets(String s) {
@@ -13,5 +16,25 @@ public class UtilityMethods {
             }
         }
         return s.substring(iter, s.length() - iter );
+    }
+    // the asymptotic of these 2 functions should be improved to O(n*log(n))
+    public static int searchLeft(ArrayList<Integer> array, int element) {
+        Collections.sort(array);
+        for (int i = array.size() - 1; i >= 0; i--) {
+            if (array.get(i) < element) {
+                return i;
+            }
+        }
+        return -1; // can not be called actually
+    }
+
+    public static int searchRight(ArrayList<Integer> array, int element) {
+        Collections.sort(array);
+        for (int i = 0; i < array.size(); i++) {
+            if (array.get(i) > element) {
+                return i;
+            }
+        }
+        return -1; // can not be called actually
     }
 }
