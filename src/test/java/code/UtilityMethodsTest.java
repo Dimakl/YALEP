0cafa23db426b420d4b2e847f4bb4b72d9c0549e
Dimakl <dimakid123@gmail.com>
2019-02-01 22:47:57 +0300

    tests added

--- src/test/java/code/UtilityMethodsTest.java
+++ src/test/java/code/UtilityMethodsTest.java
@@ -1,2 +1,19 @@
+package code;
+
+import org.junit.Test;
+import org.junit.Assert;
+
 public class UtilityMethodsTest {
+    @Test
+    public void testManyBrackets() {
+        String inp = "((((A&B))))", res = "A&B";
+        Assert.assertEquals(UtilityMethods.deleteBrackets(inp), res);
+    }
+
+    @Test
+    public void testNoBrackets() {
+        String inp = "A&B", res = "A&B";
+        Assert.assertEquals(UtilityMethods.deleteBrackets(inp), res);
+    }
+
 }
