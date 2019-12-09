196267f1efef143719d7e4489cb6f539dccd3bb9
Dimakl <dimakid123@gmail.com>
2019-12-10 00:28:41 +0300

    update converter with better checker

--- src/main/java/yalep/code/ExpressionToLogicalGatesConverter.java
+++ src/main/java/yalep/code/ExpressionToLogicalGatesConverter.java
@@ -1,4 +1,19 @@
 package yalep.code;
 
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import yalep.antlr.generated.LogicalExpressionLexer;
+import yalep.antlr.generated.LogicalExpressionParser;
+import yalep.exceptions.WrongExpressionFormatException;
+
 public class ExpressionToLogicalGatesConverter {
+
+    private String expression;
+    private Integer answer;
+
+    public ExpressionToLogicalGatesConverter(String expression) throws WrongExpressionFormatException {
+        LogicalExpressionValidityChecker.expressionIsValid(expression);
+        this.expression = expression.substring(0, expression.indexOf("="));
+        answer = Integer.parseInt(expression.substring(expression.indexOf("=") + 1));
+    }
 }
