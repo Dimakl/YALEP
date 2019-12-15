923991aea470f785669b4de894b932a7de7fe7ac
Dimakl <dimakid123@gmail.com>
2019-12-15 21:21:26 +0300

    enum for treenodes

---
+++ src/main/java/yalep/code/generate_image/AbstractTreeGenerator.java
@@ -0,0 +1,12 @@
+package yalep.code;
+
+public class AbstractTreeGenerator {
+
+    private String expression;
+
+    public AbstractTreeGenerator(String expression) {
+        this.expression = expression;
+    }
+
+
+}
---
+++ src/main/java/yalep/code/generate_image/TreeNodeType.java
@@ -0,0 +1,9 @@
+package yalep.code.generate_image;
+
+public enum TreeNodeType {
+    VAR,
+    OR,
+    AND,
+    PARENS,
+    XOR
+}
