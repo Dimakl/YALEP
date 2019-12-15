e0b0c179609b55dd02a7c5714283bc1c030f7c3a
Dimakl <dimakid123@gmail.com>
2019-12-15 22:45:03 +0300

    add setup for operator images

--- src/main/java/yalep/code/generate_image/operator/OperatorImages.java
+++ src/main/java/yalep/code/generate_image/operator/OperatorImages.java
@@ -1,4 +1,41 @@
-package yalep.code.generate_image;
+package yalep.code.generate_image.operator;
+
+import yalep.code.generate_image.TreeNodeType;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class OperatorImages {
+
+    private final static String OPERATOR_IMAGES_PATH = "/operators/";
+    private static Map<TreeNodeType, Image> nodeTypeToImage = new HashMap();
+
+    public static void setup(OperatorStandards standard) {
+        String fullPath = OPERATOR_IMAGES_PATH + standard.getFolder();
+        setupNodeTypeToImage(fullPath);
+    }
+
+    public Image getOperatorImageByNodeType(TreeNodeType nodeType) {
+        return nodeTypeToImage.get(nodeType);
+    }
+
+    private static void setupNodeTypeToImage(String fullPath) {
+        try {
+            nodeTypeToImage = new HashMap() {{
+                for (TreeNodeType nodeType : TreeNodeType.values()) {
+                    if (nodeType != TreeNodeType.VAR) {
+                        Image image = ImageIO.read(getClass().getResource(fullPath + nodeType.getImageFileName()));
+                        put(nodeType, image);
+                    }
+                nodeTypeToImage.put(TreeNodeType.VAR, null);
+                }
+            }};
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
