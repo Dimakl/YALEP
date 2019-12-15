2860cbc351be6bd9682876bbbb3058d504f891d1
Dimakl <dimakid123@gmail.com>
2019-12-15 19:27:58 +0300

    add custom listener for catching ambiguities

--- src/main/java/yalep/antlr/LogicalExpressionErrorListener.java
+++ src/main/java/yalep/antlr/LogicalExpressionErrorListener.java
@@ -1,4 +1,23 @@
 package yalep.antlr;
 
-public class LogicalExpressionErrorListener {
+import org.antlr.v4.runtime.BaseErrorListener;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.atn.ATNConfigSet;
+import org.antlr.v4.runtime.dfa.DFA;
+
+import java.util.BitSet;
+
+public class LogicalExpressionErrorListener extends BaseErrorListener {
+
+    @Override
+    public void reportAmbiguity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, boolean exact, BitSet ambigAlts, ATNConfigSet configs) {
+        FailedRecognizers.addFailedRecognizer(recognizer);
+    }
+
+    @Override
+    public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
+        FailedRecognizers.addFailedRecognizer(recognizer);
+    }
 }
