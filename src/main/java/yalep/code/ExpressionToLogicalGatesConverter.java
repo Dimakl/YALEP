97f3c033b28eb1a1ff476e12df1ba8c25b092bc2
Dimakl <dimakid123@gmail.com>
2019-12-15 22:45:34 +0300

    additional call in converter func

--- src/main/java/yalep/code/ExpressionToLogicalGatesConverter.java
+++ src/main/java/yalep/code/ExpressionToLogicalGatesConverter.java
@@ -4,6 +4,8 @@ import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import yalep.antlr.generated.LogicalExpressionLexer;
 import yalep.antlr.generated.LogicalExpressionParser;
+import yalep.code.generate_image.operator.OperatorImages;
+import yalep.code.generate_image.operator.OperatorStandards;
 import yalep.exceptions.WrongExpressionFormatException;
 
 public class ExpressionToLogicalGatesConverter {
@@ -11,8 +13,9 @@ public class ExpressionToLogicalGatesConverter {
     private String expression;
     private Integer answer;
 
-    public ExpressionToLogicalGatesConverter(String expression) throws WrongExpressionFormatException {
+    public ExpressionToLogicalGatesConverter(String expression, OperatorStandards operatorStandard) throws WrongExpressionFormatException {
         expression = expression.replaceAll(" ","");
+        OperatorImages.setup(operatorStandard);
         LogicalExpressionValidityChecker.expressionIsValid(expression);
         this.expression = expression.substring(0, expression.indexOf("="));
         answer = Integer.parseInt(expression.substring(expression.indexOf("=") + 1));
